15f733d18175f96312420004ee5b98e3f3541f94
JerickTwO <jerickjjtm774@gmail.com>
2026-01-29 19:14:44 -0500

    fix: Corregir la redacción del director de Ingeniería Civil en el componente PdfFiveTwo

--- src/pages/Apps/Steps/Pdfs/pdf-5-2.jsx
+++ src/pages/Apps/Steps/Pdfs/pdf-5-2.jsx
@@ -42,7 +42,7 @@ const PdfFiveTwo = ({ infoStep, institutionalInfo }) => {
 
             <View style={styles.section}>
                 <Text style={styles.justify}>
-                    En la Oficina de la Unidad de Investigación,  {institutionalInfo.directorIngenieriaCivil}  siendo las {horaInicio} del día {fechaActa}, bajo la convocatoria del Director de la Unidad de Investigación, a través de la CARTA
+                    En la Oficina de la Unidad de Investigación, el director {institutionalInfo.directorIngenieriaCivil}  siendo las {horaInicio} del día {fechaActa}, bajo la convocatoria del Director de la Unidad de Investigación, a través de la CARTA
                     MULTIPLE Nº {cartaMultiple}-D-UI-FA-UNAMBA, de fecha {cartaFecha} y CARTA MULTIPLE Nº {segundoCartaMultiple}-D-UI-FA-UNAMBA, de fecha {segundaCartaFecha}, se realiza el sorteo de
                     jurados, de la tesis titulada: "{title}", presentado por {getBachillersLabel(FIRST_STEP_INFO?.student, FIRST_STEP_INFO?.studentTwo)}:
                 </Text>
