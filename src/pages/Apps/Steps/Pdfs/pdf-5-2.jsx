209ce1b57216790e3b3072a87d0aa7a15571c4d2
JerickTwO <jerickjjtm774@gmail.com>
2026-01-29 19:30:02 -0500

    fix: Corregir redacción en el componente PdfFiveTwo para mejorar la claridad en la descripción del sorteo de jurados

--- src/pages/Apps/Steps/Pdfs/pdf-5-2.jsx
+++ src/pages/Apps/Steps/Pdfs/pdf-5-2.jsx
@@ -41,7 +41,7 @@ const PdfFiveTwo = ({ infoStep, institutionalInfo }) => {
 
             <View style={styles.section}>
                 <Text style={styles.justify}>
-                    En la Oficina de la Unidad de Investigación, siendo las {horaInicio} del día {fechaActa}, bajo la convocatoria del Director de la Unidad de Investigación,  el director {institutionalInfo?.directorIngenieriaCivil} a través de la CARTA
+                        En la Oficina de la Unidad de Investigación, siendo las {horaInicio} del día {fechaActa}, bajo la convocatoria del Director de la Unidad de Investigación, {institutionalInfo?.directorIngenieriaCivil} a través de la CARTA
                     MULTIPLE Nº {cartaMultiple}-D-UI-FA-UNAMBA, de fecha {fechaActa} y CARTA MULTIPLE Nº {segundoCartaMultiple}-D-UI-FA-UNAMBA, de fecha {segundaCartaFecha}, se realiza el sorteo de
                     jurados, de la tesis titulada: "{title}", presentado por {getBachillersLabel(FIRST_STEP_INFO?.student, FIRST_STEP_INFO?.studentTwo)}:
                 </Text>
